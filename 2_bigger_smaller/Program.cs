7c9d4a9abae9b972a7b91ba810d44f37e34898ff
Mari <654321.85@mail.ru>
2023-07-17 15:36:34 +0300

    commit

---
+++ 2_bigger_smaller/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и
+// выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите число 1: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int y = int.Parse(Console.ReadLine());
+
+if (x>y)
+{
+    Console.WriteLine($"Число {x} больше числа {y} ");
+}
+else 
+{
+    Console.WriteLine($"Число {y} больше числа {x} ");
+}
---
+++ 4_max_of_3_numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и 
+//выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите число 1: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int y = int.Parse(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int z = int.Parse(Console.ReadLine());
+
+if (x>y&&x>z)
+{
+    Console.WriteLine($"Число {x} является наибольшим из введенных чисел");
+}
+else if (y>x&&y>z)
+{
+    Console.WriteLine($"Число {y} является наибольшим из введенных чисел");
+}
+else
+{
+    Console.WriteLine($"Число {z} является наибольшим из введенных чисел");
+}
